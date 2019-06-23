c7fca08aa525b4b62ddc7bcdcfe8c4a1fd6fe7ba
Abdou2004 <45575757+AbdouDz09@users.noreply.github.com>
2019-06-23 14:30:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const ticket = require('./Data/tickets.json');
 const client = new Discord.Client({disableEveryone: true});
 const config = require('./Data/config.json')
-const prefix = config.prefix;
+const prefix = "_";
 const fs = require('fs');
 const dateFormat = require('dateformat');
 const setc = {}
@@ -353,4 +353,39 @@ message.channel.sendEmbed(embed)
 }
 });
 
-client.login(config.token);
+var adminprefix = "_";
+const devs = ['461468630773661699','466734861558743041'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+	
+     if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
+     
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else
+	if (message.content.startsWith(adminprefix + 'sett')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/abdoufersaoui_dz");
+    message.channel.sendMessage(`تم تغيير تويتش البوت إلى  ${argresult}`)
+	}
+});
+
+
+client.login(process.env.BOT_TOKEN);
